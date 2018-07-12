8950e0b2459c685a82d2479e1b45aac3c097e672
Anshit Chaudhary <dranrox@gmail.com>
2018-07-12 16:23:58 +0530

    still

--- resources/views/layouts/login-sign-up.blade.php
+++ resources/views/layouts/login-sign-up.blade.php
@@ -6,7 +6,7 @@
     <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
     <link rel="shortcut icon" href="img/ico/favicon.png">
-
     <link rel="stylesheet" href="css/style-login-sign-up.css">
+    <script src="js/login.js"></script>
 
 </head>
